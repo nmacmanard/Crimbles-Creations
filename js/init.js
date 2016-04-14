3b3c9ff39fb20b7d3d11eaa55c1d2276e72822c1
Daniel Wildman <iWildman1@hotmail.com>
2016-04-14 16:23:16 +0100

    Created login page

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 /*Space bar*/
-$("input").focus(function() {
+$("#input-nolog").focus(function() {
   $(this).animate({width:"520px", backgroundColor: "#FFF"}, 500);
 }).blur(function(){
   $(this).animate({width: "300px", backgroundColor: "#f0f0f0"}, 500);
